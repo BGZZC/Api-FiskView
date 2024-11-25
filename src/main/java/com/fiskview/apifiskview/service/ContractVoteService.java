db49d2c461ecf114e6d93f60e678453127eed56d
Facundoo15 <fangulos1@upao.edu.pe>
2024-11-25 00:14:29 -0500

    config: Clase de configuración web3j

---
+++ src/main/java/com/fiskview/apifiskview/service/ContractVoteService.java
@@ -0,0 +1,28 @@
+package com.fiskview.apifiskview.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.exceptions.TransactionException;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.response.PollingTransactionReceiptProcessor;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class ContractVoteService {
+    
+
+}
